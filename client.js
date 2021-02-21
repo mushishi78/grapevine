bf1977fbf01a4a169d267754a9bad4d3f1fe55e2
Max White <max@zaptic.com>
2021-02-21 09:56:14 +0000

    Basic joinning and connecting

--- client.js
+++ client.js
@@ -1,7 +1,70 @@
-const io = require('socket.io-client')
+const io = require("socket.io-client");
+const React = require("react");
+const ReactDom = require("react-dom");
 
-const socket = io();
-
-socket.on('connect', () => {
-    console.log('connected')
+window.addEventListener("load", () => {
+  ReactDom.render(
+    React.createElement(App, {}),
+    document.querySelector("#root")
+  );
 });
+
+function App() {
+  const [room, setRoom] = React.useState({ status: "connecting" });
+
+  React.useEffect(() => {
+    // Todo: Redirect if pathname is less than 4 characters
+
+    const socket = io();
+
+    socket.on("connect", () => {
+      console.log("Connected:", socket.id);
+      socket.emit("join", location.pathname);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected");
+      setRoom({ status: "connecting" });
+    });
+
+    socket.on("joined", (newPlayerId, room) => {
+      console.log("New player: ", newPlayerId);
+      setRoom(room);
+    });
+
+    socket.on("left", (oldPlayerId, room) => {
+      console.log("Player left: ", oldPlayerId);
+      setRoom(room);
+    });
+  }, []);
+
+  if (room.status === "connecting") {
+    // prettier-ignore
+    return div('full-page', {},
+      div('full-page-box', {},
+        div('full-page-message', {}, 'Connecting....')));
+  }
+
+  if (room.status === "loby") {
+    // prettier-ignore
+    return div('loby', {},
+      div('players', {},
+        room.players.map(player =>
+          div('player', { key: player.id }, player.id))))
+  }
+}
+
+//
+// React helpers
+
+function div(className, props, ...children) {
+  return React.createElement("div", { className, ...props }, ...children);
+}
+
+function a(className, href, props, ...children) {
+  return React.createElement("a", { className, href, ...props }, ...children);
+}
+
+function img(className, src, props, ...children) {
+  return React.createElement("img", { className, src, ...props }, ...children);
+}
--- server.js
+++ server.js
@@ -1,30 +1,67 @@
-const http = require('http')
-const SocketIO = require('socket.io')
-const static = require('node-static');
-const fs = require('fs')
+const http = require("http");
+const SocketIO = require("socket.io");
+const static = require("node-static");
+const fs = require("fs");
 
-const fileServer = new static.Server('./static', { cache: null });
-const indexHTML = fs.readFileSync(__dirname + '/static/index.html', 'utf8');
+const fileServer = new static.Server("./static", { cache: null });
+const indexHTML = fs.readFileSync(__dirname + "/static/index.html", "utf8");
 
 const httpServer = http.createServer((request, response) => {
-  request.addListener('end', () => fileServer.serve(request, response, err => {
-    if (err && err.status === 404) {
-      response.setHeader('Content-Type', 'text/html');
-      response.setHeader('Content-Length', Buffer.byteLength(indexHTML));
-      response.statusCode = 200;
-      response.end(indexHTML);
-    }
-  })).resume();
+  request
+    .addListener("end", () =>
+      fileServer.serve(request, response, (err) => {
+        if (err && err.status === 404) {
+          response.setHeader("Content-Type", "text/html");
+          response.setHeader("Content-Length", Buffer.byteLength(indexHTML));
+          response.statusCode = 200;
+          response.end(indexHTML);
+        }
+      })
+    )
+    .resume();
 
-  request.on('error', err => console.error(err.stack))
+  request.on("error", (err) => console.error(err.stack));
 });
+
+const rooms = new Map();
 
 const io = SocketIO(httpServer);
 
-io.on('connection', socket => {
-  console.log('connect');
+io.on("connection", (socket) => {
+  console.log("connected:", socket.id);
+
+  socket.on("join", (pathname) => {
+    // Add player to room map
+    const room = rooms.get(pathname) || { status: "loby", players: [] };
+    room.players = room.players.concat({ id: socket.id });
+    rooms.set(pathname, room);
+
+    // Connect to socket room
+    socket.join(pathname);
+
+    // Broadcast that player has joined
+    io.to(pathname).emit("joined", socket.id, room);
+  });
+
+  socket.on("disconnecting", () => {
+    for (const pathname of socket.rooms) {
+      // Ignore private room
+      if (pathname === socket.id) continue;
+
+      // Get room that socket was part of
+      const room = rooms.get(pathname);
+      if (room == null) continue;
+
+      // Remove player from room
+      room.players = room.players.filter((p) => p.id !== socket.id);
+      rooms.set(pathname, room);
+
+      // Broadcast to other players
+      socket.broadcast.to(pathname).emit("left", socket.id, room);
+    }
+  });
 });
 
 httpServer.listen(3000, () => {
-  console.log('Listening on http://localhost:3000');
+  console.log("Listening on http://localhost:3000");
 });
